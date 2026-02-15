5d7ac582708304d7e86d9b78735419eaf8dd288f
abdallah <abood.99.10.osa@gmail.com>
2026-02-15 10:22:49 +0200

    wip - add libraries for vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>home page</div>
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade-slide" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
+
+<style>
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: all 0.3s ease;
+}
+
+.fade-slide-enter-from {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateX(-20px);
+}
+</style>
